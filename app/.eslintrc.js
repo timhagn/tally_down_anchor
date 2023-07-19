08b57c3b92221251296f3fe70d345a2649c1ae6a
Tim Hagn <mail@timhagn.com>
2023-07-19 11:21:46 +0000

    add web3 deps & switch to devnet

---
+++ app/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2022: true,
+    node: true,
+  },
+  extends: ['next/core-web-vitals', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'react-hooks'],
+  root: true,
+  rules: {
+    '@typescript-eslint/no-non-null-assertion': 0,
+    '@typescript-eslint/ban-ts-comment': 0,
+    '@typescript-eslint/no-useless-escape': 0,
+    'react-hooks/rules-of-hooks': 'warn',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/no-unescaped-entities': 'warn',
+  },
+}
---
+++ app/target/types/tally_down.ts
@@ -0,0 +1,251 @@
+export type TallyDown = {
+  "version": "0.1.0",
+  "name": "tally_down",
+  "instructions": [
+    {
+      "name": "initialize",
+      "accounts": [
+        {
+          "name": "tokeAccount",
+          "isMut": true,
+          "isSigner": true
+        },
+        {
+          "name": "systemProgram",
+          "isMut": false,
+          "isSigner": false
+        },
+        {
+          "name": "tokeSave",
+          "isMut": true,
+          "isSigner": false
+        }
+      ],
+      "args": []
+    },
+    {
+      "name": "toke",
+      "accounts": [
+        {
+          "name": "tokeAccount",
+          "isMut": true,
+          "isSigner": true
+        },
+        {
+          "name": "tokeSave",
+          "isMut": true,
+          "isSigner": false
+        }
+      ],
+      "args": [
+        {
+          "name": "lastMidnight",
+          "type": "i64"
+        }
+      ]
+    },
+    {
+      "name": "backFillTokes",
+      "accounts": [
+        {
+          "name": "tokeAccount",
+          "isMut": true,
+          "isSigner": true
+        },
+        {
+          "name": "tokeSave",
+          "isMut": true,
+          "isSigner": false
+        }
+      ],
+      "args": [
+        {
+          "name": "tokes",
+          "type": {
+            "vec": {
+              "defined": "Tokes"
+            }
+          }
+        }
+      ]
+    }
+  ],
+  "accounts": [
+    {
+      "name": "tokeSave",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "tokeAccount",
+            "type": "publicKey"
+          },
+          {
+            "name": "currentTokeTime",
+            "type": "i64"
+          },
+          {
+            "name": "currentTokeCount",
+            "type": "i8"
+          },
+          {
+            "name": "tokes",
+            "type": {
+              "vec": {
+                "defined": "Tokes"
+              }
+            }
+          },
+          {
+            "name": "bump",
+            "type": "u8"
+          }
+        ]
+      }
+    }
+  ],
+  "types": [
+    {
+      "name": "Tokes",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "tokeDate",
+            "type": "i64"
+          },
+          {
+            "name": "tokeCount",
+            "type": "i8"
+          }
+        ]
+      }
+    }
+  ]
+};
+
+export const IDL: TallyDown = {
+  "version": "0.1.0",
+  "name": "tally_down",
+  "instructions": [
+    {
+      "name": "initialize",
+      "accounts": [
+        {
+          "name": "tokeAccount",
+          "isMut": true,
+          "isSigner": true
+        },
+        {
+          "name": "systemProgram",
+          "isMut": false,
+          "isSigner": false
+        },
+        {
+          "name": "tokeSave",
+          "isMut": true,
+          "isSigner": false
+        }
+      ],
+      "args": []
+    },
+    {
+      "name": "toke",
+      "accounts": [
+        {
+          "name": "tokeAccount",
+          "isMut": true,
+          "isSigner": true
+        },
+        {
+          "name": "tokeSave",
+          "isMut": true,
+          "isSigner": false
+        }
+      ],
+      "args": [
+        {
+          "name": "lastMidnight",
+          "type": "i64"
+        }
+      ]
+    },
+    {
+      "name": "backFillTokes",
+      "accounts": [
+        {
+          "name": "tokeAccount",
+          "isMut": true,
+          "isSigner": true
+        },
+        {
+          "name": "tokeSave",
+          "isMut": true,
+          "isSigner": false
+        }
+      ],
+      "args": [
+        {
+          "name": "tokes",
+          "type": {
+            "vec": {
+              "defined": "Tokes"
+            }
+          }
+        }
+      ]
+    }
+  ],
+  "accounts": [
+    {
+      "name": "tokeSave",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "tokeAccount",
+            "type": "publicKey"
+          },
+          {
+            "name": "currentTokeTime",
+            "type": "i64"
+          },
+          {
+            "name": "currentTokeCount",
+            "type": "i8"
+          },
+          {
+            "name": "tokes",
+            "type": {
+              "vec": {
+                "defined": "Tokes"
+              }
+            }
+          },
+          {
+            "name": "bump",
+            "type": "u8"
+          }
+        ]
+      }
+    }
+  ],
+  "types": [
+    {
+      "name": "Tokes",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "tokeDate",
+            "type": "i64"
+          },
+          {
+            "name": "tokeCount",
+            "type": "i8"
+          }
+        ]
+      }
+    }
+  ]
+};
